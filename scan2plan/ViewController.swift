a2b4421ec09bff2a059f8af27d431521d149482a
jookimmy <30672093+jookimmy@users.noreply.github.com>
2018-10-06 13:53:59 -0500

    first commit

---
+++ scan2plan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scan2plan
+//
+//  Created by Joowon Kim on 10/6/18.
+//  Copyright © 2018 CS196Illinois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
