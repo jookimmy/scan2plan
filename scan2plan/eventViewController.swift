4cd0e270230762efbd2a91ce3f7adbf61bab9a9d
damanm24 <dmulye2@illinois.edu>
2018-10-20 18:11:33 -0500

    Untested event adder

---
+++ scan2plan/eventViewController.swift
@@ -0,0 +1,60 @@
+//
+//  eventViewController.swift
+//  scan2plan
+//
+//  Created by Mulye, Daman on 10/20/18.
+//  Copyright © 2018 CS196Illinois. All rights reserved.
+//
+
+import UIKit
+import EventKit
+
+class eventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func btnEventAdd(_ sender: Any) {
+        let eventStore:EKEventStore = EKEventStore()
+        
+        eventStore.requestAccess(to: .event, completion: {(granted, error) in
+            if(granted && error == nil) {
+                print("Access granted. \(granted)")
+                print("Error: \(String(describing: error))")
+                
+                let event:EKEvent = EKEvent(eventStore: eventStore)
+                event.title = "Test event title"
+                event.startDate = Date()
+                event.endDate = Date()
+                event.notes = "Some description of the event"
+                event.calendar = eventStore.defaultCalendarForNewEvents
+                do {
+                    try eventStore.save(event, span: .thisEvent)
+                } catch let error as NSError {
+                    print(error)
+                }
+            } else {
+                print("error: \(error)")
+            }
+        })
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
