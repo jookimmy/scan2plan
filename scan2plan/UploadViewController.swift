f8e323f412f30dd9eaf8083890265d405ac696a3
shoeferg <srf3@illinois.edu>
2018-11-04 19:15:05 -0600

    hey

--- scan2plan/UploadViewController.swift
+++ scan2plan/UploadViewController.swift
@@ -11,24 +11,23 @@ import AVFoundation
 import Vision
 import Photos
 
-class UploadViewController: UIViewController, UIImagePickerControllerDelegate, AVCapturePhotoCaptureDelegate, UINavigationControllerDelegate {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: Outlets
-    let imagePicker = UIImagePickerController()
+    let image = UIImagePickerController()
     @IBOutlet weak var uploadView: UIImageView!
     @IBAction func uploadButton(_ sender: Any) {
-//        let image = UIImagePickerController()
-//        image.delegate = self
-        self.imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
-        imagePicker.allowsEditing = false
-        self.present(imagePicker, animated: true)
+        image.delegate = self
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = false
+        self.present(image, animated: true)
         {
             
         }
     }
     
-    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             uploadView.image = image
         } else {
             print("error")
@@ -38,7 +37,7 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, A
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        image.delegate = self
         // Do any additional setup after loading the view.
     }
     
