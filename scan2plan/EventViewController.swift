191a52e7903c8a4b134a3b77ada83486cb34f4fc
damanm24 <dmulye2@illinois.edu>
2018-11-05 19:19:14 -0600

    Implemented Rudimentary address and date time extraction

--- scan2plan/EventViewController.swift
+++ scan2plan/EventViewController.swift
@@ -21,12 +21,62 @@ class EventViewController: UIViewController {
         startDateTimeField.date = Date()
         titleTextField.text = "Parsed text"
         locationTextField.text = "Parsed location"
+        informationExtractor()
+        
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func informationExtractor() {
+        let eventString = detectedText
+        let range = NSRange(eventString.startIndex..<eventString.endIndex, in: eventString)
+        let detectionTypes: NSTextCheckingResult.CheckingType = [.date, .address]
+        let detector = try NSDataDetector(types: detectionTypes.rawValue)
+        detector.enumerateMatches(in: eventString, options: [], range: range) { (match, flags, _) in
+            guard let match = match else {
+                return
+            }
+            
+            switch match.resultType {
+            case .date:
+                let detectedDate = match.date
+                print(detectedDate)
+                startDateTimeField.date = detectedDate
+            case .address:
+                if let components = match.components {
+                    var addressComponents = [components[.name], components[.street], components[.city], components[.state], components[.zip], components[.country]]
+                    var addressString = ""
+                    for c in addressComponents {
+                        if c == nil {
+                            continue
+                        }
+                        addressComponents.append(" ")
+                        addressComponents.append(c)
+                    }
+                    locationTextField.text = addressString
+                }
+            default:
+                return
+            }
+        }
+//        let dataDetector = NSDataDetector(types: detectionTypes.rawValue, error: nil)
+//        dataDetector?.enumerateMatchesInString(detectedText, options: nil, range: NSMakeRange(0, eventString.length)) { (match, flags, _) in
+//            let matchString = eventString.substringWithRange(match.rnage)
+//            if match.resultType == .Date {
+//                println("Matched Date: \(matchString); \n- Date: \(match.date)")
+//            } else if match.resultType == .Address {
+//                if let addressComponents = match.addressComponents as NSDictionary? {
+//                    println("Match: \(matchString); \n- Street: \(addressComponents[NSTextCheckingStreetKey]);\n- Zip: \(addressComponents[NSTextCheckingZIPKey])")
+//                }
+//            } else {
+//                println("Match: \(matchString)")
+//            }
+//        }
+        
     }
     
     func createDate(year: Int, month: Int, day: Int, hour: Int, minute: Int) -> Date {
@@ -54,9 +104,10 @@ class EventViewController: UIViewController {
                 print("Error: \(String(describing: error))")
                 
                 let event:EKEvent = EKEvent(eventStore: eventStore)
-                event.title = "Daman's Birthday"
-                event.startDate = createDate(2018, 12, 24, 0, 0)
-                event.endDate = createDate(2018, 12, 24, 0, 0)
+                event.title = titleTextField.text
+                event.startDate = startDateTimeField.date
+                event.endDate = startDateTimeField.date + 1800 //1800 seconds is the equivelant to 30 minutes
+                event.location = locationTextField.text
                 event.notes = "Just a test of date creation"
                 event.calendar = eventStore.defaultCalendarForNewEvents
                 do {
