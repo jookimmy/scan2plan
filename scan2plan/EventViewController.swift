e126a6a8a3a11e868a6a6aa636a6af075d2b8e1d
Mulye <dmulye2@ug-mac39.ics.illinois.edu>
2018-11-10 17:47:29 -0600

    Reset UI

--- scan2plan/EventViewController.swift
+++ scan2plan/EventViewController.swift
@@ -12,32 +12,7 @@ import Firebase
 import FirebaseMLVision
 
 class EventViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-//    @IBOutlet weak var titleTextField: UITableViewCell!
-//    @IBOutlet weak var locationTextField: UITableViewCell!
-//    @IBOutlet weak var startDateTimeField: UIDatePicker!
-    
-    let pickerAnimationDuration = 0.40 // duration for the animation to slide the date picker into view
-    let datePickerTag           = 99   // view tag identifiying the date picker view
-    
-    let titleKey = "title" // key for obtaining the data source item's title
-    let dateKey  = "date"  // key for obtaining the data source item's date value
-    
-    // keep track of which rows have date cells
-    let dateStartRow = 1
-    let dateEndRow   = 2
-    
-    let dateCellID       = "dateCell";       // the cells with the start or end date
-    let datePickerCellID = "datePickerCell"; // the cell containing the date picker
-    let otherCellID      = "otherCell";      // the remaining cells at the end
-    var dataArray: [[String: AnyObject]] = []
-    var dateFormatter = DateFormatter()
-    
-    // keep track which indexPath points to the cell with UIDatePicker
-    var datePickerIndexPath: NSIndexPath?
-    
-    var pickerCellRowHeight: CGFloat = 216
-    // Passed from PreviewViewController
+
     var detectedText = String()
     var visionText: VisionText!
     
@@ -54,42 +29,12 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
             print(block.text)
             print(block.frame.size.height)
         }
-        
-        // Do any additional setup after loading the view.
-        
-        // setup our data source
-        let itemOne = [titleKey : "Tap a cell to change its date:"]
-        let itemTwo = [titleKey : "Start Date", dateKey : NSDate()] as [String : Any]
-        let itemThree = [titleKey : "End Date", dateKey : NSDate()] as [String : Any]
-        let itemFour = [titleKey : "(other item1)"]
-        let itemFive = [titleKey : "(other item2)"]
-        dataArray = [itemOne as Dictionary<String, AnyObject>, itemTwo as Dictionary<String, AnyObject>, itemThree as Dictionary<String, AnyObject>, itemFour as Dictionary<String, AnyObject>, itemFive as Dictionary<String, AnyObject>]
-        
-        dateFormatter.dateStyle = .short // show short-style date format
-        dateFormatter.timeStyle = .short
-        
-        // if the local changes while in the background, we need to be notified so we can update the date
-        // format in the table view cells
-        //
-        NotificationCenter.default.addObserver(self, selector: #selector(DateCellTableViewController.localeChanged(notif:)), name: NSLocale.currentLocaleDidChangeNotification, object: nil)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let myCell = tableView.dequeueReusableCell(withIdentifier: "DateCell", for: indexPath)
-        return myCell
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
     }
     
     func detectEventName() {
@@ -102,16 +47,6 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
             }
         }
         titleTextField.text = blockWithMax.capitalized
-        print()
-        print()
-        print()
-        print("THIS IS THE MAX HEIGHT: ")
-        print(maxHeight)
-        print("THIS IS TEXT WITH MAX HEIGHT: ")
-        print(blockWithMax)
-        print()
-        print()
-        print()
     }
     func informationExtractor() {
         let charsToRemove: Set<Character> = Set("|{}[]()".characters)
@@ -129,14 +64,6 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
                 switch match.resultType {
                 case .date:
                     let detectedDate = match.date
-//                    print()
-//                    print()
-//                    print()
-//                    print("THIS IS THE DATE")
-//                    print(match.date)
-//                    print()
-//                    print()
-//                    print()
                     startDateTimeField.date = detectedDate!
                 case .address:
                     if let components = match.components {
@@ -161,27 +88,6 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
         } catch {
             return
         }
-        
-        
-        
-        
-        
-        
-        
-//        let dataDetector = NSDataDetector(types: detectionTypes.rawValue, error: nil)
-//        dataDetector?.enumerateMatchesInString(detectedText, options: nil, range: NSMakeRange(0, eventString.length)) { (match, flags, _) in
-//            let matchString = eventString.substringWithRange(match.rnage)
-//            if match.resultType == .Date {
-//                println("Matched Date: \(matchString); \n- Date: \(match.date)")
-//            } else if match.resultType == .Address {
-//                if let addressComponents = match.addressComponents as NSDictionary? {
-//                    println("Match: \(matchString); \n- Street: \(addressComponents[NSTextCheckingStreetKey]);\n- Zip: \(addressComponents[NSTextCheckingZIPKey])")
-//                }
-//            } else {
-//                println("Match: \(matchString)")
-//            }
-//        }
-        
     }
     
     func createDate(year: Int, month: Int, day: Int, hour: Int, minute: Int) -> Date {
@@ -234,12 +140,6 @@ class EventViewController: UIViewController, UITableViewDataSource, UITableViewD
 
     /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+*/
 
 }
